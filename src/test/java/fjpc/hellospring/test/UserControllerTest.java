f076d466bb32bfd41ff95bcffc3b432e3ff32a21
Javier Pérez <fjperezcostas@gmail.com>
2019-07-10 20:28:40 +0200

    Integration Test added

---
+++ src/test/java/fjpc/hellospring/test/UserControllerTest.java
@@ -0,0 +1,43 @@
+package fjpc.hellospring.test;
+
+import fjpc.hellospring.Application;
+import fjpc.hellospring.ApplicationUser;
+import fjpc.hellospring.ApplicationUserController;
+import fjpc.hellospring.ApplicationUserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void givenApplicationUsers_whenGetAllApplicationUsers_thenReturnJsonArray() throws Exception {
+        mvc.perform(get("/users").contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].name", is("Pepe")))
+                .andExpect(jsonPath("$[1].name", is("Juan")))
+                .andExpect(jsonPath("$[2].name", is("Pedro")));
+    }
+
+}
