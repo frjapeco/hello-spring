3241b8d60c8d37b925a200b340cc536b3ef54aa8
Javier Pérez <fjperezcostas@gmail.com>
2019-07-10 23:31:01 +0200

    Optimizing imports

--- src/test/java/fjpc/hellospring/test/UserControllerTest.java
+++ src/test/java/fjpc/hellospring/test/UserControllerTest.java
@@ -1,22 +1,15 @@
 package fjpc.hellospring.test;
 
-import fjpc.hellospring.Application;
-import fjpc.hellospring.ApplicationUser;
-import fjpc.hellospring.ApplicationUserController;
-import fjpc.hellospring.ApplicationUserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
